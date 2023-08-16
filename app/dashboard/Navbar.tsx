00c4ac2e3fc53a38569fa2dd65f4d8f053055ebc
pikalupps <putudhikap@gmail.com>
2023-08-16 08:48:49 +0700

    add basic navbar

--- app/dashboard/Navbar.tsx
+++ app/dashboard/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const navbar = () => {
+const Navbar: React.FC = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <nav className="bg-blue-50 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-black font-bold text-xl">PLN UIP NUSRA</div>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="#" className="text-black hover:text-sky-300">Home</a>
+          </li>
+          <li>
+            <a href="#" className="text-black hover:text-sky-300">About</a>
+          </li>
+          <li>
+            <a href="#" className="text-black hover:text-sky-300">Services</a>
+          </li>
+          <li>
+            <a href="#" className="text-black hover:text-sky-300">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default navbar
+export default Navbar;
