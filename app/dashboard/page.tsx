91a48c30585e25d38e0f78b01efecf4984337e6c
pikalupps <putudhikap@gmail.com>
2023-08-23 09:40:31 +0700

    Add room layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,10 @@
-import Navbar from "@/components/Navbar"
-import Card from "@/components/Card"
+import Card from "@/components/UnitCard"
 import { Unit } from '@/constant'
-import Image from "next/image"
 
 export default function Page() {
   return(
     <>
-      <head>
-        <title>Dashboard</title>
-      </head>
-      <body>
-        <div className="bg">
-          <div className="fixed">
-            <Navbar />
-          </div>
+      <div className="bg">
           <div className="p-20 text-center">
             <h1 className="title pt-32 pb-5">Selamat Datang di Web Portal</h1>
             <h1 className="title pb-20">PT PLN (Persero) UIP Nusa Tenggara</h1>
@@ -21,16 +12,15 @@ export default function Page() {
         </div>
         <div className="text-center">
           <h1 className="section-title">Unit Kami</h1>
-          {<div className='flex gap-10 justify-center px-20'>
+          {<div className='lg:flex flex-1 gap-10 justify-center px-20'>
               {Unit.map((unit) => (
-                <Card alt={unit.alt} src={unit.src} address={unit.address} city={unit.city}/>
+                <Card alt={unit.alt} src={unit.src} address={unit.address} city={unit.city} telp={unit.telp} map={unit.map}/>
               ))}
           </div>}
         </div>
         <div className="text-center">
           <h1 className="section-title">Map</h1>
         </div>
-      </body>
     </>
   )
 }
